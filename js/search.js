930afeb2da365f96a27eef1882c02424b7722f83
keelezibel <letang.boon@gmail.com>
2017-07-30 02:24:16 +0000

    add new files

--- js/search.js
+++ js/search.js
@@ -20,7 +20,7 @@
   function getQueryVariable(variable) {
     var query = window.location.search.substring(1);
     var vars = query.split('&');
-    console.log(vars)
+
     for (var i = 0; i < vars.length; i++) {
       var pair = vars[i].split('=');
 
@@ -31,7 +31,7 @@
   }
 
   var searchTerm = getQueryVariable('query');
-
+  console.log(searchTerm)
   if (searchTerm) {
     document.getElementById('search-box').setAttribute("value", searchTerm);
 
